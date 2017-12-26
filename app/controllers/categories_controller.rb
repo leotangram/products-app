bf9d12acc6ac2b0c063e734ee179c2461cc2e7f0
leotangram <leotangram@gmail.com>
2017-12-26 12:05:00 -0500

    Eliminando Categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -35,6 +35,13 @@ class CategoriesController < ApplicationController
 	  end
 	end
 
+	def destroy
+	  @category = Category.find(params[:id])
+	  @category.destroy
+	 
+	  redirect_to categories_path
+	end
+
 	private
 	  def category_params
 	    params.require(:category).permit(:name)
