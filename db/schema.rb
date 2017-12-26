d75f7bcc7723fafc0a67a65232a154b9214f3411
leotangram <leotangram@gmail.com>
2017-12-26 12:58:52 -0500

    Creando modelo Product

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171226153720) do
+ActiveRecord::Schema.define(version: 20171226175704) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
