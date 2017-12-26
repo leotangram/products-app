1f2fb599ac5dc751bdbb998555b8ed55a948c3ec
leotangram <leotangram@gmail.com>
2017-12-26 13:15:39 -0500

    Mostrando y listando Productos

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,24 @@
 class ProductsController < ApplicationController
+	def index
+	  @products = Product.all
+	end
+
+	def show
+	  @product = Product.find(params[:id])
+	end
+
 	def new
-		
 	end
 
 	def create
-		render plain: params[:product].inspect
+		@product = Product.new(product_params)
+		
+		@product.save
+		redirect_to @product
 	end
+
+	private
+	  def product_params
+	    params.require(:product).permit(:name, :price)
+	  end
 end
